bbf3ba9f2a4edb4171b22fd7f618097d4ffafa77
GeronimoYT <gero_salvi@hotmail.com>
2020-08-31 21:42:25 -0300

    Pantalla Principal

---
+++ Principal/Principal/Ventanas/Ingreso.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Principal
+{
+    partial class FormIngresar
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.LblUsuario = new System.Windows.Forms.Label();
+            this.LblContrasena = new System.Windows.Forms.Label();
+            this.BtnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(167, 58);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(152, 23);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContrasena.Location = new System.Drawing.Point(167, 122);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '*';
+            this.txtContrasena.Size = new System.Drawing.Size(152, 23);
+            this.txtContrasena.TabIndex = 1;
+            // 
+            // LblUsuario
+            // 
+            this.LblUsuario.AutoSize = true;
+            this.LblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblUsuario.Location = new System.Drawing.Point(58, 61);
+            this.LblUsuario.Name = "LblUsuario";
+            this.LblUsuario.Size = new System.Drawing.Size(68, 20);
+            this.LblUsuario.TabIndex = 2;
+            this.LblUsuario.Text = "Usuario:";
+            // 
+            // LblContrasena
+            // 
+            this.LblContrasena.AutoSize = true;
+            this.LblContrasena.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblContrasena.Location = new System.Drawing.Point(58, 125);
+            this.LblContrasena.Name = "LblContrasena";
+            this.LblContrasena.Size = new System.Drawing.Size(96, 20);
+            this.LblContrasena.TabIndex = 3;
+            this.LblContrasena.Text = "Contraseña:";
+            // 
+            // BtnIngresar
+            // 
+            this.BtnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIngresar.Location = new System.Drawing.Point(244, 201);
+            this.BtnIngresar.Name = "BtnIngresar";
+            this.BtnIngresar.Size = new System.Drawing.Size(75, 32);
+            this.BtnIngresar.TabIndex = 4;
+            this.BtnIngresar.Text = "Ingresar";
+            this.BtnIngresar.UseVisualStyleBackColor = true;
+            this.BtnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
+            // 
+            // FormIngresar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 245);
+            this.Controls.Add(this.BtnIngresar);
+            this.Controls.Add(this.LblContrasena);
+            this.Controls.Add(this.LblUsuario);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "FormIngresar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ingresar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.Label LblUsuario;
+        private System.Windows.Forms.Label LblContrasena;
+        private System.Windows.Forms.Button BtnIngresar;
+    }
+}
+
---
+++ Principal/Principal/Ventanas/Ingreso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Principal
+{
+    public partial class FormIngresar : Form
+    {
+        public FormIngresar()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            string user = "Geronimo";
+            string pass = "gerito22";
+
+            if (txtUsuario.Text == user && txtContrasena.Text == pass)
+                MessageBox.Show("Bienvenido " + txtUsuario.Text);
+            else MessageBox.Show("Ingrese un usaurio y contraseña válido!");
+            
+        }
+    }
+}
