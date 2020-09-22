1312bd7f0bb2251b94a074e13fca62adf1ef8dab
arielx22 <arielecaliva@gmail.com>
2020-09-22 02:11:01 -0300

    clases

---
+++ Principal/Principal/Clases/Repositorio/PasajerosRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal.Clases.Repositorio
+{
+    class PasajerosRepositorio
+    {
+        public List<Pasajero> ObtenerPasajeros()
+        {
+            List<Pasajero> pasajeros = new List<Pasajero>();
+            var sentenciaSql = "SELECT * FROM Pasajero";
+            var tabla = DBHelper.GetDBHelper().ConsultaSQL(sentenciaSql);
+            foreach (DataRow fila in tabla.Rows)
+            {
+                var pasajero = new Pasajero();
+                pasajero.TipoDocumento = fila["TipoDNI"].ToString();
+                pasajero.NroDocumento = fila["NroDNI"].ToString();
+                pasajero.NroTelefono = fila["Telefono"].ToString();
+                pasajero.Mail = fila["Mail"].ToString();
+
+                pasajeros.Add(pasajero);
+            }
+            return pasajeros;
+
+        }
+    }
+}
---
+++ Principal/Principal/Clases/Repositorio/PasajesRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal.Clases.Repositorio
+{
+    class PasajesRepositorio
+    {
+        public List<Pasaje> ObtenerPasajes()
+        {
+            List<Pasaje> pasajes = new List<Pasaje>();
+            var sentenciaSql = "SELECT * FROM Pasaje";
+            var tabla = DBHelper.GetDBHelper().ConsultaSQL(sentenciaSql);
+            foreach (DataRow fila in tabla.Rows)
+            {
+                var pasaje = new Pasaje();
+                pasaje.Id = Convert.ToInt32(fila["NroPasaje"].ToString());
+                pasaje.Precio = Convert.ToInt32(fila["Precio"].ToString());
+                pasaje.Descripcion = fila["NroPasaje"].ToString();
+
+                pasajes.Add(pasaje);
+            }
+            return pasajes;
+        }
+    }
+}
