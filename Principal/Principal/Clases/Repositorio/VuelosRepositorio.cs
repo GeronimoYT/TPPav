86cea41589cff62f3f1cd136c80fe2cf820ff6eb
ElielVilte <39287652+ElielVilte@users.noreply.github.com>
2020-10-30 00:36:51 -0300

    a recursar !

---
+++ Principal/Principal/Clases/Repositorio/VuelosRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal.Clases.Repositorio
+{
+    class VuelosRepositorio
+    {
+        public List<Vuelo> Obtenervuelos()
+        {
+            List<Vuelo> vuelos = new List<Vuelo>();
+            var sentenciaSql = "SELECT * FROM Vuelo";
+            var tabla = DBHelper.GetDBHelper().ConsultaSQL(sentenciaSql);
+            foreach (DataRow fila in tabla.Rows)
+            {
+                var vuelo = new Vuelo();
+                vuelo.NroVuelo = Convert.ToInt32(fila["NroVuelo"].ToString());
+                vuelo.FechaHoraSalida = Convert.ToDateTime(fila["FechaHoraSalida"].ToString());
+                vuelo.FechaHoraLlegada = Convert.ToDateTime(fila["FechaHoraLlegada"].ToString());
+                vuelo.NroAvion = Convert.ToInt32(fila["NroAvion"].ToString());
+                vuelo.IdTipoAvion = fila["IdTipoAvion"].ToString();
+                //vuelo.IdTipoAvion = Convert.ToInt32(fila["IdtipoAvion"].ToString());
+                vuelo.IdAeropuerto = Convert.ToInt32(fila["IdAeropuerto"].ToString());
+                vuelo.IdAeropuertoDestino = Convert.ToInt32(fila["IdAeropuertoDestino"].ToString());
+                vuelo.Estado = Convert.ToInt32(fila["Estado"].ToString());
+
+                vuelos.Add(vuelo);
+            }
+            return vuelos;
+
+        }
+
+    }
+}
--- Principal/Principal/Ventanas/Aviones/Aviones.cs
+++ Principal/Principal/Ventanas/Aviones/Aviones.cs
@@ -110,5 +110,6 @@ namespace Principal.Ventanas
         {
             this.Close();
         }
+        
     }
 }
